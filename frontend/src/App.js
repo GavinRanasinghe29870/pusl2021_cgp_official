5cb32c01e1bbd329241771125c2b0d36b32cfbdc
GavinRanasinghe29870 <gavinkisara@gmail.com>
2025-02-23 13:59:26 +0530

    0011-Gavin
    Make frontend for product manage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/sportPeople/Home';
 import ProductList from "./components/sportPeople/ProductList";
+import ProductManage from './components/admin/ProductManage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<ProductList/>} />
+        <Route path="/admin/productManaging" element={<ProductManage />} />
       </Routes>
     </Router>
   );
