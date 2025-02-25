81f01a5ac5cf14c3901ac0b805259ab4f578c704
Thisuri08 <tgamage@students.nsbm.ac.lk>
2025-02-25 12:47:51 +0530

    0013
    Added frontend of SingleProduct

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProductList from "./components/sportPeople/ProductList";
 import ProductManage from './components/admin/ProductManage';
 import Signin from './components/sportPeople/Signin';
 import 'react-multi-carousel/lib/styles.css';
+import SingleProduct from './components/sportPeople/Singleproduct';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/shop" element={<ProductList/>} />
         <Route path="/admin/productManaging" element={<ProductManage />} />
         <Route path="/Signin" element={<Signin />} />
+        <Route path='/singleproduct' element={<SingleProduct />} />
       </Routes>
     </Router>
   );
