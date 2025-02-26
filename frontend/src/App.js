1960158cc66d44a158261b5f8635950c2c5d5314
agasthi23 <137294353+agasthi23@users.noreply.github.com>
2025-02-26 23:04:25 +0530

    0013
    Added Donation Request page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/sportPeople/Home';
 import ProductList from "./components/sportPeople/ProductList";
 import ProductManage from './components/admin/ProductManage';
 import Signin from './components/sportPeople/Signin';
+import DonationRequestForm from './components/clubs/donationReq';
 import 'react-multi-carousel/lib/styles.css';
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/shop" element={<ProductList/>} />
         <Route path="/admin/productManaging" element={<ProductManage />} />
         <Route path="/Signin" element={<Signin />} />
+        < Route path="/donationReq" element={<DonationRequestForm/>}/>
       </Routes>
     </Router>
   );
