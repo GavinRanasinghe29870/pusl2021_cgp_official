346af69956dfa7ca0019dd038bf3c42f3b14819c
GavinRanasinghe29870 <gavinkisara@gmail.com>
2025-03-10 02:16:22 +0530

    0020-Gavin
    1. Create frontend of Product insertion part and Shop page.
    2. Move some frontend component files to component folder and product manage page to page folder.
    3. Link frontend and backend in shop page and product insert part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/sportPeople/Home';
-import ProductList from "./components/sportPeople/ProductList";
-import ProductManage from './components/admin/ProductManage';
+import ProductList from "./pages/clubs/ProductList";
+import ProductManage from './pages/admin/ProductManage';
 import Signin from './components/sportPeople/Signin';
 import DonationRequestForm from './components/clubs/donationReq';
 import 'react-multi-carousel/lib/styles.css';
-import InsertProduct from './pages/admin/InsertProductPage';
+import InsertProduct from './components/admin/InsertProduct';
+import ProductPage from './pages/sportPeople/ProductsPage';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<ProductList/>} />
+        <Route path="/shop" element={<ProductPage />} />
         <Route path="/admin/productManaging" element={<ProductManage />} />
         <Route path="/Signin" element={<Signin />} />
         < Route path="/donationReq" element={<DonationRequestForm/>}/>
