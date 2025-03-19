b683ef150725b8647935c52e4fa25f18f0a4942f
Dulith Rajapaksha <140060255+dulithrajapaksha@users.noreply.github.com>
2025-03-19 09:49:30 +0530

    Club
    Frontend pages
    Club portfolio
    Club Maker
    Checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/sportPeople/Home';
 import ProductList from "./components/sportPeople/ProductList";
 import Signin from './components/sportPeople/Signin';
+import ClubPortfolio from "./components/clubs/ClubPortfolio";
 import 'react-multi-carousel/lib/styles.css';
+import ClubMakerPage from './components/clubs/Clubmaker';
+import CheckoutPage  from './components/clubs/Checkout';
 
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<ProductList/>} />
         <Route path="/Signin" element={<Signin />} />
+        <Route path="/ClubPortfolio" element={<ClubPortfolio />}/>
+        <Route path="/Clubmaker" element={<ClubMakerPage />}/>
+        <Route path="/Checkout" element={<CheckoutPage />}/>
       </Routes>
     </Router>
   );
