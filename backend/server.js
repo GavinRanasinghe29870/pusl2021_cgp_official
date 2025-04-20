85d059e2ea85430ef0e938aae9ecb3527a95bc3c
agasthi23 <137294353+agasthi23@users.noreply.github.com>
2025-04-20 11:07:25 +0530

    0081
    updated friendchat.js

--- backend/server.js
+++ backend/server.js
@@ -40,8 +40,8 @@ const memberRoutes = require("./routes/clubs/memberRoutes");
 const SingleProductRoutes = require("./routes/sportPeople/SingleProductRoutes");
 const registrationApprovalRoutes = require("./routes/clubs/registrationApprovalRoutes");
 const ClubAuth = require("./routes/clubs/ClubAuth.js");
-const donatingRoutes = require("./routes/sportPeople/donatingRoutes"); //
-
+const donatingRoutes = require("./routes/sportPeople/donatingRoutes");//
+const friendRoutes= require("./routes/sportPeople/friendRoutes.js")
 const jwt = require("jsonwebtoken");
 
 const token = jwt.sign({ userId: "12345" }, process.env.JWT_SECRET, {
@@ -61,17 +61,18 @@ app.use("/public/uploads", express.static("uploads"));
 
 //Link Signin Authentication Routes
 app.use("/api/auth", authRoutes);
+
 // ✅ Mount Admin Auth Routes
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/donation", donationRoutes);
 app.use("/api/req", memberRoutes);
-app.use("/api/donating", donatingRoutes); //
+app.use("/api/donating", donatingRoutes);//
 
 app.use("/api/message", messageRoutes);
 app.use("/api/ClubAuth", ClubAuth);
-
+app.use("/api/friendmsg",friendRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.use("/api/singleproduct", SingleProductRoutes);
@@ -89,8 +90,6 @@ app.use("/uploads", express.static("uploads"));
 app.use((req, res, next) => {
   res.status(404).json({ message: "API endpoint not found" });
 });
-
-
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
